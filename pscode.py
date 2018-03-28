84fe55ec83abe914716c3e0e318090837e0ecb41
gmkanik <gmkanik@terpmail.umd.edu>
2018-03-28 10:37:06 -0400

    make keys secret

--- pscode.py
+++ pscode.py
@@ -1,1 +1,14 @@
 import tweepy
+import json
+
+#opens and reads mvkey.json
+pskey={}
+with open("keys/pskey.json") as file:
+    pskey = json.loads(file.read())
+    
+# Consumer keys and access tokens, used for OAuth
+consumer_key = pskey["consumer_key"]
+consumer_secret = pskey["consumer_secret"]
+access_token = pskey["access_token"]
+access_token_secret = pskey["access_token_secret"]
+    
